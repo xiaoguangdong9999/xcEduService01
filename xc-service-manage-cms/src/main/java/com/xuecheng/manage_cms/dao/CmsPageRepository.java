14d91a212113d1e83fd9b24ca569b441c14c2025
chenbin <3375793023@qq.com>
2019-11-22 16:22:14 +0800

    页面的增删改查

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/dao/CmsPageRepository.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/dao/CmsPageRepository.java
@@ -7,4 +7,13 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface CmsPageRepository extends MongoRepository<CmsPage,String> {
     //根据页面名称查询
     CmsPage findByPageName(String pageName);
+
+    /**
+     * 根据页面名称、站点ID，页面访问路劲查询
+     * @param pageName
+     * @param siteId
+     * @param pageWebPath
+     * @return
+     */
+    CmsPage findByPageNameAndSiteIdAndPageWebPath(String pageName,String siteId,String pageWebPath);
 }
--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -2,15 +2,18 @@ package com.xuecheng.manage_cms.service;
 
 import com.xuecheng.framework.domain.cms.CmsPage;
 import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.domain.cms.response.CmsPageResult;
 import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.QueryResponseResult;
 import com.xuecheng.framework.model.response.QueryResult;
+import com.xuecheng.framework.model.response.ResponseResult;
 import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * @author Administrator
@@ -26,27 +29,98 @@ public class PageService {
 
     /**
      * 页面查询方法
-     * @param page 页码，从1开始记数
-     * @param size 每页记录数
+     *
+     * @param page             页码，从1开始记数
+     * @param size             每页记录数
      * @param queryPageRequest 查询条件
      * @return
      */
-    public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest){
+    public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest) {
 
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching().withMatcher("pageAliase",ExampleMatcher.GenericPropertyMatchers.contains());
+        CmsPage cmsPage = new CmsPage();
+        if (StringUtils.isNotEmpty(cmsPage.getSiteId())) {
+            cmsPage.setSiteId(cmsPage.getPageId());
+        }
+        if (StringUtils.isNotEmpty(queryPageRequest.getPageAliase())) {
+            cmsPage.setPageAliase(queryPageRequest.getPageAliase());
+        }
+
+        Example example = Example.of(cmsPage,exampleMatcher);
         //分页参数
-        if(page <=0){
+        if (page <= 0) {
             page = 1;
         }
-        page = page -1;
-        if(size<=0){
+        page = page - 1;
+        if (size <= 0) {
             size = 10;
         }
-        Pageable pageable = PageRequest.of(page,size);
-        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        Page<CmsPage> all = cmsPageRepository.findAll(example,PageRequest.of(page,size));
         QueryResult queryResult = new QueryResult();
         queryResult.setList(all.getContent());//数据列表
         queryResult.setTotal(all.getTotalElements());//数据总记录数
-        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS,queryResult);
+        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS, queryResult);
         return queryResponseResult;
+    }
+
+    /**
+     * 校验页面是否存在
+     * @param cmsPage
+     * @return
+     */
+    public CmsPageResult add (CmsPage cmsPage) {
+        CmsPage cmsPage1 = cmsPageRepository.findByPageNameAndSiteIdAndPageWebPath(cmsPage.getPageName(),cmsPage.getSiteId(),cmsPage.getPageWebPath());
+        if (cmsPage1 == null) {
+            cmsPage.setPageId(null);
+            cmsPageRepository.save(cmsPage);
+            CmsPageResult cmsPageResult = new CmsPageResult(CommonCode.SUCCESS,cmsPage);
+            return cmsPageResult;
+        }
+        return new CmsPageResult(CommonCode.FAIL,null);
+    }
+
+    /**
+     * 根据ID查询页面
+     * @param id
+     * @return
+     */
+    public CmsPageResult findById(String id) {
+        Optional<CmsPage> optional = cmsPageRepository.findById(id);
+        if (optional.isPresent()) {
+            CmsPageResult cmsPageResult = new CmsPageResult(CommonCode.SUCCESS,optional.get());
+            return cmsPageResult;
+        }
+        return new CmsPageResult(CommonCode.FAIL,null);
+    }
+
+    public CmsPageResult update (String id ,CmsPage cmsPage) {
+        CmsPageResult cmsPageResult1 = this.findById(id);
+
+        if (cmsPageResult1.isSuccess()) {
+            CmsPage one =  cmsPageResult1.getCmsPage();
+            one.setPageId(cmsPage.getPageId());
+            one.setPageAliase(cmsPage.getPageAliase());
+            one.setSiteId(cmsPage.getSiteId());
+            one.setDataUrl(cmsPage.getDataUrl());
+            one.setPageName(cmsPage.getPageName());
+            one.setPagePhysicalPath(cmsPage.getPagePhysicalPath());
+            one.setPageWebPath(cmsPage.getPageWebPath());
+            CmsPage save =  cmsPageRepository.save(one);
+            if (save != null) {
+                CmsPageResult cmsPageResult = new CmsPageResult(CommonCode.SUCCESS,save);
+                return cmsPageResult;
+            }
+        }
+        return new CmsPageResult(CommonCode.FAIL,null);
+    }
+
+
+    public ResponseResult delete(String id) {
+        CmsPageResult cmsPageResult =  this.findById(id);
+        if (cmsPageResult.isSuccess()) {
+            cmsPageRepository.deleteById(id);
+            return new ResponseResult(CommonCode.SUCCESS);
+        }
+        return new ResponseResult(CommonCode.FAIL);
     }
 }
