b973e0da6ff41aff1970a1de424774ad95cc5c42
chenbin <3375793023@qq.com>
2019-12-09 14:59:48 +0800

    v1.6 release

---
+++ xc-service-api/src/main/java/com/xuecheng/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.xuecheng.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author User
+ * @date 2019/12/9
+ * @description
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+
+    }
+}
